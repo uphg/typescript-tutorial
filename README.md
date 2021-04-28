2979e14392068ac75933d480b2fe87eddf9bfdef
ChenNing02 <2633565885@qq.com>
2021-04-28 15:38:07 +0800

    声明函数返回值类型

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 ## 使用 interface
 
-```javascript
+```typescript
 interface Person {
   firstName: string;
   lastName: string;
@@ -17,7 +17,7 @@ console.log(greeter({ firstName: '小', lastName: '明' })) // Hello, 小明
 
 ## 使用 class
 
-```javascript
+```typescript
 class Student {
   姓名: string;
   constructor(public 姓, public 名) {
@@ -29,4 +29,16 @@ let user = new Student("小", "红");
 
 console.log('# user')
 console.log(user) // {姓: '小', 名: '红', 姓名: '小红'}
+```
+
+## 声明函数返回值类型
+
+```typescript
+function min(a:number, b:number): number {
+  return a > b ? b : a
+}
+
+const result = min(1, 2)
+
+console.log(result)
 ```
--- hi.ts
+++ hi.ts
@@ -1,39 +1,7 @@
-class Student {
-  fullName: string;
-  constructor(public firstName, public middleInitial, public lastName) {
-    this.fullName = firstName + " " + middleInitial + " " + lastName;
-  }
+function min(a:number, b:number): number {
+  return a > b ? b : a
 }
 
-/**
- * 等同于如下 JS： 
- * 
- * class Student {
- *   fullName: string;
- * 
- *   firstName: any;
- *   middleInitial: any;
- *   lastName: any;
- * 
- *   constructor(firstName, middleInitial, lastName) {
- *     this.firstName = firstName
- *     this.middleInitial = middleInitial
- *     this.lastName = lastName
- * 
- *     this.fullName = firstName + " " + middleInitial + " " + lastName;
- *   }
- * }
- */
+const result = min(1, 2)
 
-interface Person {
-  firstName: string;
-  lastName: string;
-}
-
-function greeter(person : Person) {
-  return "Hello, " + person.firstName + " " + person.lastName;
-}
-
-let user = new Student("Jane", "#", "User");
-
-console.log(greeter(user))
+console.log(result)
