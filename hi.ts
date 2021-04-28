f89e98d1a5144fa41fc483467c992da5cb8768d5
ChenNing02 <2633565885@qq.com>
2021-04-28 15:00:33 +0800

    添加 interface 使用示例

--- hi.ts
+++ hi.ts
@@ -1,1 +1,10 @@
-console.log('hi')
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  return "Hello，" + person.firstName + " and " + person.lastName
+}
+
+console.log(greeter({ firstName: 'Jack', lastName: 'Bark' }))
